bcf89346f9f19c6a526b23912ff65fe14f3ca6d0
Mahadev Singh <mahadevsingh625@gmail.com>
2023-10-16 22:27:01 +0530

    second pattern

--- git.java
+++ git.java
@@ -14,6 +14,18 @@ public class git {
                     System.out.print(" ");
                 }
             }
+            
+            for(int j=0; j<n; j++)
+            {
+                if(i+j>=(n-1)/2 && j-i<=(n-1)/2 && i<=(n-1)/2 || i-j<=(n-1)/2 && i+j<=(n-1)/2+(n-1) && i>=(n-1)/2 )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
              
             System.out.println();
         }
